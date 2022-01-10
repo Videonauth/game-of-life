e443b159f34058e250daa8b48342bd07a21bd7a7
Videonauth <videonauth@googlemail.com>
2022-01-10 01:20:09 +0100

    added file `modules/colour.py` and moved `Colour()` to it.

---
+++ modules/colour.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# ---------------------------------------------------------------------------
+# game-of-life - colour
+# ---------------------------------------------------------------------------
+# Author: Videonauth <videonauth@googlemail.com>
+# License: MIT (see LICENSE file)
+# Date: 10.01.22 - 01:09
+# Purpose: -
+# Written for: Python 3.6.3
+# ---------------------------------------------------------------------------
+
+"""Colour definition."""
+
+
+class Colour:
+    """Containing all colour definitions."""
+
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+
+if __name__ == '__main__':
+    pass
