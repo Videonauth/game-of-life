39f591c8ac178880b27337d14e1dea1a40a64a77
Videonauth <videonauth@googlemail.com>
2022-01-12 00:26:45 +0100

    code cleanup.

--- modules/gui.py
+++ modules/gui.py
@@ -12,14 +12,17 @@
 
 """gui class."""
 
+from modules.colour import Colour
 import pygame
+
+colours = Colour()
 
 
 class GUI:
-    """Lorem ipsum dolor sit amet ..."""
+    """GUI class contains everything belonging to output and the window."""
 
     def __init__(self, caption: str, window_size: tuple, fps: int):
-        """Ipsum dolor lorem sit amet ..."""
+        """Initialize the graphical user interface."""
         # storing window dimensions for internal use
         self._window_size = window_size
         self._window_width = window_size[0]
@@ -32,39 +35,42 @@ class GUI:
         # create and name window
         self.window = pygame.display.set_mode(window_size)
         pygame.display.set_caption(caption)
+        # setup default flush colour
+        self._flush_colour = colours.black
         # setup font
         self.font = pygame.font.SysFont(None, 20, False, False)
 
     def flush(self):
-        """Dolor ipsum lorem sit amet ..."""
+        """Fills the whole window output screen with a chosen colour."""
         # flush output
-        self.window.fill((0, 0, 0))
+        self.window.fill(self._flush_colour)
 
     def add_button(self,
                    label: str,
-                   colour: tuple = (255, 255, 255),
+                   colour: Colour = colours.black,
                    top_x: int = 0,
                    top_y: int = 0,
                    width: int = 420,
                    height: int = 40,
-                   background_image: str = '',
-                   background_colour: tuple = (-1, -1, -1)
+                   background_image: pygame.Surface = None,
+                   background_colour: Colour = None
                    ) -> tuple:
-        """implicate that there are docstrings outside ..."""
+        """Draw a button on the output screen and return the clickable border positions absolute."""
         surface = pygame.Surface((width, height))
-        if not background_colour == (-1, -1, -1):
+        # fill if we got a colour we fill the button with it
+        if background_colour is not None:
             surface.fill(background_colour)
-        if not background_image == '':
-            # fixme: image should not be loaded in a function which might be called in the game loop
-            _image = pygame.image.load(background_image)
-            # todo: insert image bound check
-            surface.blit(_image, (0, 0))
-        # draw the button
+        # draw an image if we got one
+        if background_image is not None:
+            surface.blit(background_image, (0, 0))
+        # draw the inner and outer border
         pygame.draw.rect(surface, colour, (0, 0, width, height), 1)
         pygame.draw.rect(surface, colour, (3, 3, width - 6, height - 6), 1)
+        # render the text centered
         text = self.font.render(label, True, colour)
         _text_boundary = text.get_rect()
         surface.blit(text, ((width / 2) - (_text_boundary.width / 2), (height / 2) - (_text_boundary.height / 2)))
+        # draw and return tuple for clickable surface positions 
         self.window.blit(surface, (top_x, top_y))
         return top_x, top_y, top_x + width, top_y + height
 
