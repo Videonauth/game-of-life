d205e4f9c82c79e294b961c3eaca3da322646d8c
Videonauth <videonauth@googlemail.com>
2022-01-12 15:16:41 +0100

    first draft of the `Playfield` class.

--- modules/playfield.py
+++ modules/playfield.py
@@ -7,6 +7,11 @@ from collections import namedtuple
 from random import sample
 from typing import List
 
+from modules.gui import colours
+from modules.simulation import simulation
+
+import pygame
+
 
 def generate_playfield(_playfield_height: int, _playfield_width: int) -> List[List[int]]:
     """
@@ -69,3 +74,60 @@ def generate_seeded_playfield(
         _playfield[_cell_to_seed.row][_cell_to_seed.cell] = 1
 
     return _playfield
+
+
+class Playfield:
+    """Playfield class contains everything done in regard of the playfield."""
+
+    def __init__(self,
+                 playfield_size: tuple[int, int],
+                 surface_size: tuple[int, int],
+                 ):
+        """Initialize the playfield class."""
+        self.width = playfield_size[0]
+        self.height = playfield_size[1]
+        self._flush_colour = colours.black
+        if not pygame.get_init():
+            pygame.init()
+        self.surface = pygame.Surface((min(surface_size[0], surface_size[1]) - 20,
+                                       min(surface_size[0], surface_size[1]) - 20))
+        self.field = generate_playfield(self.height, self.width)
+        self.cell_size = (min(surface_size[0], surface_size[1]) - 20) // max(self.width, self.height)
+
+    def flush_surface(self):
+        """Flush the output surface."""
+        self.surface.fill(self._flush_colour)
+
+    def flip_cell(self, cell_x, cell_y):
+        """Flip a playfield cell from set to unset and vice versa."""
+        self.field[cell_y][cell_x] = self.field[cell_y][cell_x] ^ 1
+
+    def clear(self):
+        """Clear the playfield, i.e. setting each cell to zero."""
+        self.field = generate_playfield(self.height, self.width)
+
+    def randomize(self, multiplier: float = 0.5):
+        """Fill the playfield with randomized cells."""
+        self.field = generate_seeded_playfield(self.height, self.width, int(self.height * self.width * multiplier))
+
+    def simulate(self):
+        """Simulate one generation step on the playfield."""
+        self.field = simulation(self.field)
+
+    def update_surface(self):
+        """Draw the actual playfield onto the output surface."""
+        # drawing playfield
+        start_x = 0
+        start_y = 0
+        for line in self.field:
+            for cell in line:
+                if cell == 0:
+                    pygame.draw.rect(self.surface, colours.white, (start_x, start_y, self.cell_size, self.cell_size), 1)
+                elif cell == 1:
+                    pygame.draw.rect(self.surface, colours.blue, (start_x, start_y, self.cell_size, self.cell_size))
+                    pygame.draw.rect(self.surface, colours.white, (start_x, start_y, self.cell_size, self.cell_size), 1)
+                else:
+                    pass
+                start_x += self.cell_size
+            start_x = 0
+            start_y += self.cell_size
